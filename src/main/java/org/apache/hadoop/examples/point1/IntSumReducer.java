df5f9b63ad7aaa80b0395abcb8569d9bfffe2c77
swilsonmelo <swilsonmelo@gmail.com>
2020-03-25 17:18:19 -0500

    init

---
+++ src/main/java/org/apache/hadoop/examples/point1/IntSumReducer.java
@@ -0,0 +1,38 @@
+package org.apache.hadoop.examples.point1;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+
+public class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	private IntWritable result = new IntWritable();
+
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
+		
+		String[] words = {"TRUMP","DICTATOR","MAGA","IMPEACH", "DRAIN","SWAP","CHANGE"};
+		for (String w : words) {
+			String k = key.toString();
+			if( k.equals(w) ) {
+				int sum = 0;
+				for (IntWritable val : values) {
+					sum += val.get();
+				}
+				result.set(sum);
+				context.write(key, result);
+			}	
+		}
+		
+	}
+}
---
+++ src/main/java/org/apache/hadoop/examples/point1/TokenizerMapper.java
@@ -0,0 +1,33 @@
+package org.apache.hadoop.examples.point1;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+	private final static IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+	public void map(Object key,Text value, Context context
+			) throws IOException, InterruptedException {		
+		String newValue = value.toString().replace("{", " ").replace("}", " ").replace("[", " ").replace("]", " ").replace("null", " ")
+				.replace(",", " ");
+		StringTokenizer itr = new StringTokenizer(newValue);
+		while (itr.hasMoreTokens()) {
+			word.set(itr.nextToken());
+			context.write(word, one);
+		}
+	}
+	
+}

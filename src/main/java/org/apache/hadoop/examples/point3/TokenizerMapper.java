3d6e8a3ab7f6789a141dd3ff6777f45d214eb755
swilsonmelo <swilsonmelo@gmail.com>
2020-03-25 19:23:25 -0500

    added jar

--- src/main/java/org/apache/hadoop/examples/point3/TokenizerMapper.java
+++ src/main/java/org/apache/hadoop/examples/point3/TokenizerMapper.java
@@ -1,6 +1,8 @@
 package org.apache.hadoop.examples.point3;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
@@ -13,29 +15,33 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
 	private final static IntWritable one = new IntWritable(1);
 	private Text word = new Text();
 
-	public void map(Object key,Text value, Context context
-			) throws IOException, InterruptedException {
-		String[] stopWords = {"a", "an", "and", "are", "etc"};
-		String newValue = value.toString().replace("{", " ").replace("}", " ").replace("[", " ").replace("]", " ").replace("null", " ")
-				.replace(",", " ");
-		StringTokenizer itr = new StringTokenizer(newValue);		
-		while (itr.hasMoreTokens()) {
-			String token = itr.nextToken();
-			boolean flag = false;
-			for (String sW : stopWords) {
-				if(token.equals(sW)) flag = true;
-			}
-			if( !flag ) {
-				word.set(token);
-				context.write(word, one);
-			}
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+		JSONParser parser = new JSONParser();
+		JSONObject json;
+		try {
+			json = (JSONObject) parser.parse(value.toString());
+
+			JSONObject user = (JSONObject) json.get("user");
+			String userName = user.get("name").toString();
+			word.set(userName);
+			context.write(word, one);
+
+		} catch (
+
+		ParseException e) {
+			System.out.println("RIP");
+			e.printStackTrace();
 		}
 	}
-	
+
 }
